6c4bae9f0151a37efe8447389fc47b841bbc29e0
hchtz <ybkk1027@foxmail.com>
2015-09-07 22:03:44 +0800

    登陆完毕后首页改为用户管理页面

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,8 @@ router.get('/', function (req, res, next) {
 router.post('/login', function (req, res, next) {
     var params = req.body;
     if (params.username == 'admin' && params.password == '123456') {
-        res.render('index', {user: {username: params.username}});
+        res.redirect('user');// 默认不在显示首页，而是直接默认显示用户管理页面
+        //res.render('index', {user: {username: params.username}});
     } else {
         res.render('login', {'msg': '用户名密码错误'});
     }
--- service/api/service.js
+++ service/api/service.js
@@ -3,6 +3,7 @@
  */
 'use strict';
 var db = require('../../config/db');
+var dateFormat = require('date-format');
 
 /**
  * 登陆
@@ -11,12 +12,17 @@ var db = require('../../config/db');
  * @param callback
  */
 module.exports.login = function (username, password, callback) {
+    if (!username) return callback({rs: false, ms: '用户名不能为空'});
+    if (username.length < 4 || username.length > 10) return callback({rs: false, ms: '用户名长度需在４到10之间'});
+    if (!password) return callback({rs: false, ms: '密码不能为空'});
+    if (password.length < 4 || password.length > 10) return callback({rs: false, ms: '密码长度需在４到10之间'});
     var loginSql = 'SELECT id,password FROM sys_user WHERE username = ?';
     db.pool.query(loginSql, username, function (error, row, field) {
         if (row && row[0]) {
-            if (password === row[0].password) {
-                callback({rs: true, ms: row[0].id});
-            } else callback({rs: false, ms: '密码错误'});
+            if (password === row[0].password) callback({rs: true, ms: row[0].id}); else callback({
+                rs: false,
+                ms: '密码错误'
+            });
         } else callback({rs: false, ms: '用户名不存在'});
     });
 };
@@ -28,24 +34,18 @@ module.exports.login = function (username, password, callback) {
  * @param callback
  */
 module.exports.register = function (username, password, callback) {
-    if(!username) callback({rs: false, ms: '用户名不能为空'});
-    if(username.length<4 || username.length>10)callback({rs: false, ms: '用户名长度需在４到10之间'});
-    if(!password) callback({rs: false, ms: '密码不能为空'});
-    if(password.length<4 || password.length>10)callback({rs: false, ms: '密码长度需在４到10之间'});
+    if (!username) return callback({rs: false, ms: '用户名不能为空'});
+    if (username.length < 4 || username.length > 10) return callback({rs: false, ms: '用户名长度需在４到10之间'});
+    if (!password)return callback({rs: false, ms: '密码不能为空'});
+    if (password.length < 4 || password.length > 10)return callback({rs: false, ms: '密码长度需在４到10之间'});
     var checkSql = 'SELECT count(*) AS count FROM sys_user WHERE username = ?';
     db.pool.query(checkSql, username, function (error, row, field) {
-        if (row && row[0] && row[0].count > 0) {
-            callback({rs: false, ms: "账号已经存在"});
-            return;
-        }// 账号重复
-        var insertSql = 'INSERT INTO sys_user(username,password,status) values (?,?,?)';
-        db.pool.query(insertSql, [username, password, 0], function (error, result) {
-            if (error) {
-                callback({rs: false, ms: '未知错误,注册失败'});
-                return;
-            }
-            var id = result.insertId;
-            callback({rs: true, ms: id});
+        if (row && row[0] && row[0].count > 0) return callback({rs: false, ms: "账号已经存在"});// 账号重复
+        var insertSql = 'INSERT INTO sys_user(username,password,cdate,status) values (?,?,?)';
+        var now = dateFormat.asString('yyy-MM-dd HH:mm:ss', new Date());
+        db.pool.query(insertSql, [username, password, now, 1], function (error, result) {
+            if (error) return callback({rs: false, ms: '未知错误,注册失败'});
+            callback({rs: true, ms: result.insertId});
         });
     });
 };
@@ -73,7 +73,10 @@ module.exports.listDelPdfId = function (callback) {
 module.exports.listPdfByPid = function (categoryId, callback) {
     var selectSql = 'SELECT * FROM dic_pdf WHERE  dic_category_id = ? AND status = 1';
     db.pool.query(selectSql, [categoryId], function (error, row, field) {
-        if (row) callback(row); else callback([]);
+        if (row) {
+            for(var i =0;i<row.length;i++) row[i].json = JSON.parse(row[i].json);
+            callback(row);
+        } else callback([]);
     });
 };
 
